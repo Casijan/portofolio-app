e962a4e0120476428adc979b5b69ed8e9f89bf30
Casijan <155375888+Casijan@users.noreply.github.com>
2025-01-18 13:55:05 +0200

    imporvement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./portofolio-website/home/home.component";
+import { HomeComponent } from './portofolio-website/home/home.component';
+import { NavbarComponent } from './portofolio-website/navbar/navbar.component';
+import { FooterComponent } from './portofolio-website/footer/footer.component';
+import { ContactComponent } from './portofolio-website/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [
+    RouterOutlet,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    ContactComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portofolio-app';
-
-  
-
 }
-
-
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,8 @@ import { AboutComponent } from './portofolio-website/about/about.component';
 import { ContactComponent } from './portofolio-website/contact/contact.component';
 
 export const routes: Routes = [
-  {
-    path: 'portofolio-website',
-    loadChildren: () =>
-      import('./portofolio-website/portofolio-website.module').then(
-        (m) => m.PortfolioWebsiteModule
-      ),
-  },
-  { path: 'portofolio-website/home', component: HomeComponent },
+  { path: 'portofolio-website', component: HomeComponent },
   { path: 'portofolio-website/about', component: AboutComponent },
   { path: 'portofolio-website/contact', component: ContactComponent },
+  { path: '**', redirectTo: 'portofolio-website', pathMatch: 'full' },
 ];
